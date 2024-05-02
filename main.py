5a14279bb7991be6bdabe79c9ed55d74bd4acea2
herve4 <77961002+herve4@users.noreply.github.com>
2024-05-02 11:52:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def connexion():
     db = sqlite3.connect("new_materiels_db")
     sql = (
         'CREATE TABLE IF NOT EXISTS materiels (id_materiels INT(15) PRIMARY KEY,designation VARCHAR(100),prix VARCHAR(10),fournisseurs '
-        'VARCHAR(100), date VARCHAR(8), service VARCHAR(80),image VARCHAR(255),codeB VARCHAR(255), codeBarText VARCHAR(20),ctq VARCHAR(255), qte INT(25));')
+        'VARCHAR(100), date VARCHAR(8), service VARCHAR(80),image VARCHAR(255) null,codeB VARCHAR(255), codeBarText VARCHAR(20),ctq VARCHAR(255), qte INT(25));')
     cursor = db.cursor()
     cursor.execute(sql)
 
@@ -169,7 +169,7 @@ class My_app(object):
                                     f.append(t)
                                     print(t)
                                 print(int("".join(f)))
-                                image.save(path_file)
+                                
                                 db = sqlite3.connect("new_materiels_db")
                                 c = db.cursor()
                                 q = f"INSERT INTO materiels(id_materiels,designation,prix,fournisseurs,date,service,image,codeB,codeBarText,ctq,qte) VALUES(?,?,?,?,?,?,?,?,?,?,?)"
