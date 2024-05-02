923267ac56c387e1a60e12070fded6363ac373e7
herve4 <77961002+herve4@users.noreply.github.com>
2024-05-02 14:15:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -349,7 +349,7 @@ class My_app(object):
 
             sql2 = 'SELECT * FROM materiels WHERE codeBarText=? ;'
             e = db.cursor()
-            if not e.execute(sql2, (articles[0],)):
+            if not e.execute(sql2, (articles[2],)):
 
                 # Inserrer les données scannées
     
